d7757eda3390936dac95cfd3032ca0228f90c5a7
Nicholas Gao <nicholas.gao96@gmail.com>
2023-12-12 01:30:50 +0100

    add temperature

--- collage_generator.py
+++ collage_generator.py
@@ -9,7 +9,9 @@ import numpy as np
 
 
 class Node:
-    def __init__(self, N, alpha_t=None, parent=None, split=None, img=None, left=None, right=None):
+    def __init__(
+        self, N, alpha_t=None, parent=None, split=None, img=None, left=None, right=None
+    ):
         self.parent = parent
         self.left = left
         self.right = right
@@ -28,13 +30,13 @@ class Node:
     @property
     def alpha(self):
         if self.img is not None:
-            return self.img.shape[1]/self.img.shape[0]
+            return self.img.shape[1] / self.img.shape[0]
         alpha_left = self.left.alpha
         alpha_right = self.right.alpha
-        if self.split == 'V':
+        if self.split == "V":
             return alpha_left + alpha_right
         else:
-            return (alpha_left * alpha_right)/(alpha_left + alpha_right)
+            return (alpha_left * alpha_right) / (alpha_left + alpha_right)
 
 
 def load_images(folder):
@@ -50,41 +52,39 @@ def load_images(folder):
     return imgs
 
 
-def find_img_pair(alpha_t, L):
+def sample_energies(energies, temperature=1):
+    energies = np.array(energies)
+    probs = np.exp(-energies / temperature)
+    probs = probs / np.sum(probs)
+    return np.random.choice(len(energies), p=probs)
+
+
+def find_img_pair(alpha_t, L, temperature=1):
     p = 0
     q = len(L) - 1
-    m = abs(L[p].alpha + L[q].alpha - alpha_t)
-    i = p
-    j = q
+    energies = []
+    pairs = []
     while p < q:
         alpha_sum = L[p].alpha + L[q].alpha
-        if alpha_sum > alpha_t:
-            if abs(alpha_sum - alpha_t) < m:
-                m = abs(alpha_sum - alpha_t)
-                i = p
-                j = q
+        energies.append(abs(alpha_sum - alpha_t))
+        pairs.append((p, q))
+        if alpha_sum >= alpha_t:
             q = q - 1
         elif alpha_sum < alpha_t:
-            if abs(alpha_sum - alpha_t) < m:
-                m = abs(alpha_sum - alpha_t)
-                i = p
-                j = q
             p = p + 1
-        else:
-            i = p
-            j = q
-            break
+    i, j = pairs[sample_energies(energies, temperature)]
     return (i, j)
 
 
-def generate_tree(L, node):
+def generate_tree(L, node, temperature):
     if node.N == 1:
-        best_fit = sorted(L, key=lambda l: abs(l.alpha - node.alpha_t))[0]
+        energies = np.array([abs(l.alpha - node.alpha_t) for l in L])
+        best_fit = L[sample_energies(energies, temperature)]
         node.img = best_fit.img
         L.remove(best_fit)
         return
     if node.N == 2:
-        i, j = find_img_pair(node.alpha_t, L)
+        i, j = find_img_pair(node.alpha_t, L, temperature)
         node.left = L[i]
         node.right = L[j]
         node.left.parent = node
@@ -92,54 +92,58 @@ def generate_tree(L, node):
         del L[j]
         del L[i]
         return
-    node.split = np.random.choice(['V', 'H'])
-    if node.split == 'V':
-        node.left = Node(parent=node, N=math.floor(
-            node.N/2), alpha_t=node.alpha_t/2)
-        node.right = Node(parent=node, N=math.ceil(
-            node.N/2), alpha_t=node.alpha_t/2)
+    node.split = np.random.choice(["V", "H"])
+    if node.split == "V":
+        node.left = Node(
+            parent=node, N=math.floor(node.N / 2), alpha_t=node.alpha_t / 2
+        )
+        node.right = Node(
+            parent=node, N=math.ceil(node.N / 2), alpha_t=node.alpha_t / 2
+        )
     else:
-        node.left = Node(parent=node, N=math.floor(
-            node.N/2), alpha_t=node.alpha_t*2)
-        node.right = Node(parent=node, N=math.ceil(
-            node.N/2), alpha_t=node.alpha_t*2)
-    generate_tree(L, node.left)
-    generate_tree(L, node.right)
+        node.left = Node(
+            parent=node, N=math.floor(node.N / 2), alpha_t=node.alpha_t * 2
+        )
+        node.right = Node(
+            parent=node, N=math.ceil(node.N / 2), alpha_t=node.alpha_t * 2
+        )
+    generate_tree(L, node.left, temperature)
+    generate_tree(L, node.right, temperature)
 
 
 def adjust_tree(node, th):
     if node.img is not None:
         return
-    if node.alpha > node.alpha_t*th:
-        node.split = 'H'
-    elif node.alpha < node.alpha_t/th:
-        node.split = 'V'
-    if node.split == 'V':
-        node.left.alpha_t = node.alpha_t/2
-        node.right.alpha_t = node.alpha_t/2
+    if node.alpha > node.alpha_t * th:
+        node.split = "H"
+    elif node.alpha < node.alpha_t / th:
+        node.split = "V"
+    if node.split == "V":
+        node.left.alpha_t = node.alpha_t / 2
+        node.right.alpha_t = node.alpha_t / 2
     else:
-        node.left.alpha_t = node.alpha_t*2
-        node.right.alpha_t = node.alpha_t*2
+        node.left.alpha_t = node.alpha_t * 2
+        node.right.alpha_t = node.alpha_t * 2
     adjust_tree(node.left, th)
     adjust_tree(node.right, th)
 
 
-def generate_and_adjust_tree(imgs, ratio, th):
+def generate_and_adjust_tree(imgs, ratio, th, temperature):
     L = [Node(N=1, img=img) for img in imgs]
     L.sort(key=lambda node: node.alpha)
     root = Node(N=len(L), alpha_t=ratio)
-    generate_tree(L, root)
+    generate_tree(L, root, temperature)
     for i in range(10):
         adjust_tree(root, th)
     return root
 
 
-def generate_best_tree(imgs, target_ratio, threshold=1e-4):
+def generate_best_tree(imgs, target_ratio, threshold=1e-4, temperature=1):
     best_tree = None
     best_error = -1
-    for th in np.arange(10)/50+0.55:
+    for th in np.arange(10) / 50 + 0.55:
         for i in range(500):
-            root = generate_and_adjust_tree(imgs, target_ratio, th)
+            root = generate_and_adjust_tree(imgs, target_ratio, th, temperature)
             if abs(root.alpha - target_ratio) < best_error or best_error == -1:
                 best_error = abs(root.alpha - target_ratio)
                 best_tree = root
@@ -151,23 +155,32 @@ def generate_best_tree(imgs, target_ratio, threshold=1e-4):
 def to_image(img, node, x, y, height):
     if node.img is not None:
         new_width = math.floor(node.alpha * height)
-        res = cv2.resize(node.img, dsize=(new_width, height),
-                         interpolation=cv2.INTER_CUBIC)
-        target_shape = img[y:height+y, x:new_width+x].shape
-        img[y:height+y, x:new_width+x] = res[:target_shape[0], :target_shape[1]]
+        res = cv2.resize(
+            node.img, dsize=(new_width, height), interpolation=cv2.INTER_CUBIC
+        )
+        target_shape = img[y : height + y, x : new_width + x].shape
+        img[y : height + y, x : new_width + x] = res[
+            : target_shape[0], : target_shape[1]
+        ]
         return
     alpha = node.alpha
     l_alpha = node.left.alpha
     r_alpha = node.right.alpha
     width = alpha * height
-    if node.split == 'V':
+    if node.split == "V":
         to_image(img, node.left, x, y, height + 2)
-        to_image(img, node.right, x +
-                 math.floor(width * l_alpha/alpha), y, height + 2)
+        to_image(
+            img, node.right, x + math.floor(width * l_alpha / alpha), y, height + 2
+        )
     else:
-        to_image(img, node.left, x, y, math.floor(height * alpha/l_alpha)+2)
-        to_image(img, node.right, x, y + math.floor(height * alpha /
-                 l_alpha), math.floor(height * alpha/r_alpha)+2)
+        to_image(img, node.left, x, y, math.floor(height * alpha / l_alpha) + 2)
+        to_image(
+            img,
+            node.right,
+            x,
+            y + math.floor(height * alpha / l_alpha),
+            math.floor(height * alpha / r_alpha) + 2,
+        )
 
 
 def is_valid_folder(parser, arg):
@@ -184,25 +197,31 @@ def is_valid_file(parser, arg):
         return arg
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Colalge generator.')
-    parser.add_argument('src_folder', help='Source image folder',
-                        default='.', type=lambda x: is_valid_folder(parser, x))
-    parser.add_argument('--output', default='collage.png',
-                        type=lambda x: is_valid_file(parser, x))
-    parser.add_argument('--width', default=4096, type=int)
-    parser.add_argument('--height', default=2048, type=int)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Colalge generator.")
+    parser.add_argument(
+        "src_folder",
+        help="Source image folder",
+        default=".",
+        type=lambda x: is_valid_folder(parser, x),
+    )
+    parser.add_argument(
+        "--output", default="collage.jpg", type=lambda x: is_valid_file(parser, x)
+    )
+    parser.add_argument("--width", default=4096, type=int)
+    parser.add_argument("--height", default=2048, type=int)
+    parser.add_argument("--temperature", default=1, type=float)
     args = parser.parse_args()
 
-    print('Loading images...')
+    print("Loading images...")
     imgs = load_images(args.src_folder)
     random.shuffle(imgs)
-    target_ratio = args.width/args.height
-    print('Generating tree...')
-    tree = generate_best_tree(imgs, target_ratio)
-    print('Building image...')
-    img = np.ones((args.height, args.width, 3), dtype=np.uint8)*255
+    target_ratio = args.width / args.height
+    print("Generating tree...")
+    tree = generate_best_tree(imgs, target_ratio, args.temperature)
+    print("Building image...")
+    img = np.ones((args.height, args.width, 3), dtype=np.uint8) * 255
     to_image(img, tree, 0, 0, args.height)
-    print('Saving output...')
+    print("Saving output...")
     cv2.imwrite(args.output, img)
-    print('Done.')
+    print("Done.")
